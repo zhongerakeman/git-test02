075546af5e5c0e2798bf49f2e642e92a38a3cef6
zhongerakeman <1960591723@qq.com>
2023-08-11 03:55:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/gittest02/GitTest02Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.gittest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTest02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/gittest02/GitTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gittest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
